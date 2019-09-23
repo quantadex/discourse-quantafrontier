536706be97e8da7bdba6bbb9ac72dae536ab804e
Quoc Le <quocble@gmail.com>
2019-09-23 14:02:11 -0700

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,20 +1,14 @@
-# name: Discourse Clyp Onebox
-# about: Adds support for embedding Clyp audio player widgets within Discourse.
-# version: 0.1
-# authors: Daniel Marquard
-# url: https://github.com/critcola/discourse-clyp-onebox
-
-class Onebox::Engine::ClypOnebox
+class Onebox::Engine::QuantaFrontierOneBox
 	include Onebox::Engine
 
-	REGEX = /^https?:\/\/clyp.it\/(\w+)/
+	REGEX = /^https?:\/\/quantafrontier.com\/research\/algorithm\/(\w+)/
 	matches_regexp REGEX
 
 	def id
 		@url.match(REGEX)[1]
 	end
-	
+
 	def to_html
-		"<iframe width=\"100%\" height=\"160\" src=\"//clyp.it/#{id}/widget\" frameborder=\"0\" allowfullscreen=\"allowfullscreen\"></iframe>"
+		"<iframe width=\"100%\" height=\"160\" src=\"//quantafrontier.com\/research\/iframe?id=#{id}\" frameborder=\"0\" allowfullscreen=\"allowfullscreen\"></iframe>"
 	end
 end
